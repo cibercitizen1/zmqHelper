58604d39a82096ad1c1ac0ac0e11bbf7a27adea6
cibercitizen1 <cibercitizen1@gmail.com>
2014-12-28 17:28:09 +0100

    minor edit

--- examples/04-REQ-broker-REP/broker.cpp
+++ examples/04-REQ-broker-REP/broker.cpp
@@ -10,13 +10,31 @@
 
 using namespace zmqHelper;
 
+/*
+ * ZeroMQ applications always start by creating a context, 
+ * and then using that for creating sockets. 
+ * In C, it's the zmq_ctx_new() call. 
+ * You should create and use exactly one context in your process. 
+ * Technically, the context is the container for all sockets 
+ * in a single process, and acts as the transport for inproc sockets, 
+ * which are the fastest way to connect threads in one process. 
+ * If at runtime a process has two contexts, 
+ * these are like separate ZeroMQ instances. 
+ * If that's explicitly what you want, OK, but otherwise remember:
+ * 
+ * Do one zmq_ctx_new() at the start of your main line code, 
+ * and one zmq_ctx_destroy() at the end.
+ */
+
 // -----------------------------------------------------------------
 // -----------------------------------------------------------------
 int main ()
 {
 
-  SocketAdaptor< ZMQ_ROUTER > frontend_ROUTER;
-  SocketAdaptor< ZMQ_DEALER > backend_DEALER;
+  zmq::context_t theContext {1};
+  // but also works each socket having its own context
+  SocketAdaptor< ZMQ_ROUTER > frontend_ROUTER {theContext};
+  SocketAdaptor< ZMQ_DEALER > backend_DEALER {theContext};
 
   frontend_ROUTER.bind ("tcp://*:8000");
   backend_DEALER.bind ("tcp://*:8001");
